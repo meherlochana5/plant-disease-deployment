ec5756afe7763a241ede50f2527ffcee7599d9ee
meherlochana5 <meherlochana5@gmail.com>
2026-04-15 23:26:04 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,20 +1,47 @@
 import tensorflow as tf
 import numpy as np
 import json
+import os
+import gdown
 from PIL import Image
 from tensorflow.keras.applications.resnet50 import preprocess_input
 
-MODEL_DIR = "models/plant_savedmodel"
+MODEL_PATH = "model.h5"
+FILE_ID = "1khzncN5QbkvQvv3JXu2iXJsnfcDVkzSW"
 
-model = tf.keras.models.load_model(MODEL_DIR, compile=False)
+# Download model if missing
+if not os.path.exists(MODEL_PATH):
+    url = f"https://drive.google.com/uc?id={FILE_ID}"
+    gdown.download(url, MODEL_PATH, quiet=False)
 
+# Load model
+model = tf.keras.models.load_model(MODEL_PATH, compile=False)
+
+# Load labels
 with open("class_indices.json", "r") as f:
     labels = json.load(f)
 
 classes = {v: k for k, v in labels.items()}
 
+def is_leaf(img):
+    arr = np.array(img.resize((224, 224)))
+
+    if arr.ndim != 3:
+        return False
+
+    r = arr[:, :, 0]
+    g = arr[:, :, 1]
+    b = arr[:, :, 2]
+
+    green_pixels = ((g > r) & (g > b)).mean()
+    return green_pixels > 0.20
+
 def predict_image(path):
     img = Image.open(path).convert("RGB")
+
+    if not is_leaf(img):
+        return "Invalid Image! Upload leaf image only.", 0
+
     img = img.resize((224, 224))
     img = np.array(img, dtype=np.float32)
     img = preprocess_input(img)
@@ -25,4 +52,11 @@ def predict_image(path):
     idx = np.argmax(pred)
     confidence = round(float(np.max(pred)) * 100, 2)
 
-    return classes[idx], confidence
+    result = classes[idx]
+
+    if "healthy" in result.lower():
+        result += " (Healthy Leaf)"
+    else:
+        result += " (Disease Detected)"
+
+    return result, confidence
